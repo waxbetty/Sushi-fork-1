850520b09efed573b836dc3562a6a90b075d999c
Daniel Deverell <daniel.deverell@gmail.com>
2021-09-23 15:45:33 -0400

    updated js and readme

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,96 +1,106 @@
 // EXAMINING AND CHANGING THE DOCUMENT  //
-// console.log('Hello from scripts.js!');
-// console.log(document);
-// console.log(document.body);
-// console.log(document.links);
+console.log("Hello from scripts.js!");
 // console.log(document.title);
 // document.title = 123;
 
-/* FIND the tags in the header in the list */
-// console.log(document.all);
-// console.log(document.all[19]);
-// document.all[19].textContent = 'Hello';
+/* 1. QUERYSELECTOR - uncomment in sequence */
+// var bodyElement = document.querySelector("body");
+/* view variable in console */
+// bodyElement.textContent = "Text content set in JavaScript";
+// bodyElement.style.borderBottom = "solid 30px red";
+/* elements in dev tools VS view source */
+/* comment out all the above */
 
-/* QUERYSELECTOR - uncomment in sequence */
-// console.log(document.querySelector('body'));
-// var bodyElement = document.querySelector('body');
-// bodyElement.textContent = 'Text content set in JavaScript';
-// bodyElement.style.borderBottom = 'solid 30px red';
+/* 2. QUERYSELECTORALL */
+// var listItems = document.querySelectorAll("li");
+/* view the variable in the console. Returns a NodeList */
 
-// QUERY SELECTOR ALL //
-// var listItems = document.querySelectorAll('li');
-// NOTE - view the variable in the console. Returns a NodeList
+/*  Gives an error - and the console log command never runs */
+// listItems.style.backgroundColor = "red";
+/* errors block the rest of the code from running */
+// console.log("This will not run b/c the line above causes a blocking error");
 
-// Gives an error - and the console log command never runs
-// listItems.style.backgroundColor = 'red';
-// console.log('This will not run b/c the line above causes a blocking error');
-
-// HERE'S how you would have to do it
-// listItems.forEach(function(listItem) {
-//   listItem.style.backgroundColor = 'red';
+/* HERE'S 3 methods that WOULD work */
+/* a. */
+// listItems.forEach(function (listItem) {
+//   listItem.style.backgroundColor = "red";
 // });
 
-// OR
-// for(var item of listItems){
-//   item.style.backgroundColor = 'green';
+/* b. */
+// for (var i of listItems) {
+//   i.style.backgroundColor = "green";
 // }
 
-// OR
-// for (var i = 0; i<listItems.length; i++){
-//   listItems[i].style.backgroundColor = 'blue'
+/* c. */
+// for (var i = 0; i < listItems.length; i++) {
+//   listItems[i].style.backgroundColor = "blue";
 // }
 
-// EVENTS //
-// FIRST - add a class of 'map' to the map anchor tag in the sidebar
-// var mapLink = document.querySelector('.map');
-// mapLink.style.borderBottom = 'solid 8px red';
+/* 3. EVENTLISTENERS */
+/* ADD a class of 'map' to the map ANCHOR tag in the sidebar */
+// var mapLink = document.querySelector(".map");
+// mapLink.style.borderBottom = "solid 8px red";
 
-// ADDEVENTLISTENER
-// mapLink.addEventListener('click', function() {
-//   console.log('It works!');
+// mapLink.addEventListener("click", function () {
+//   console.log("It works!");
 // });
 
-// mapLink.addEventListener('click', function(event) {
+/* ADD a external link to the map ANCHOR tag in the sidebar */
+
+// mapLink.addEventListener("click", function (event) {
 //   event.preventDefault();
-//   console.log('It works!');
+//   console.log("It works!");
 // });
 
-// mapLink.addEventListener('click', function() {
+// mapLink.addEventListener("click", function() {
 //   event.preventDefault();
-//   console.log('event::', event);
-//   console.log('event.target::', event.target);
-//   console.log('event.target.href::', event.target.href);
+//   console.log("event::", event);
+//   console.log("event.target::", event.target);
+//   console.log("event.target.href::", event.target.href);
 // });
 
-// CALLING A FUNCTION - click twice
-// mapLink.addEventListener('click', buttonClick);
+// document.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   console.log("event.target::", e.target);
+//   console.log("event.target.textContent::", e.target.textContent);
+// });
 
-// function buttonClick() {
-//   event.preventDefault();
-//   console.log('className::', event.target.className);
-//   event.target.className = 'foo'
+/* 4. FUNCTIONS */
+// var mapLink = document.querySelector(".map");
+// mapLink.addEventListener("click", buttonClick);
+
+// function buttonClick(e) {
+//   e.preventDefault();
+//   console.log("className::", e.target.className);
+//   e.target.className = "foo";
 // }
 
-// DYNAMICALLY CREATE AN ELEMENT //
+/*
+.foo {
+  font-size: 200px;
+  color: rebeccapurple;
+}
+*/
 
-// // CREATE a div and then look at it in the console
-// var newDiv = document.createElement('div');
+/* 5. DYNAMICALLY CREATE AN ELEMENT */
 
-// // Add class
-// newDiv.className = 'popover';
+/* CREATE a div and then look at it in the console */
+// var newDiv = document.createElement("div");
+
+/* Add a class */
+// newDiv.className = "popover";
 // console.log(newDiv);
 
-// // Add text
-// newDiv.innerText = 'Hello World';
-// newDiv.innerHTML = '<span>Hello Universe</span>';
+/* Add text */
+// newDiv.innerText = "Hello World";
 
-// // Add styles to the div
-// newDiv.style.fontSize = '40px';
-// newDiv.style.color = '#600';
+/* Add styles to the div */
+// newDiv.style.fontSize = "40px";
+// newDiv.style.color = "#600";
 
-// // Store a reference to the H1
-// var headerOne = document.querySelector('header h1');
+/* Store a reference to the H1 */
+// var headerOne = document.querySelector("header h1");
 
-// // Add the new div to the document
+/* Add the new div to the document */
 // headerOne.append(newDiv);
 // headerOne.prepend(newDiv);
