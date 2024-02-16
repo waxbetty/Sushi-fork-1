41c378bcedff92cab8a37ac0eb3fe491671f3f81
waxbetty <mollybettybrown@gmail.com>
2024-02-15 20:34:50 -0500

    added popover

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,107 +1,14 @@
-// EXAMINING AND CHANGING THE DOCUMENT  //
-// console.log("Hello from scripts.js!");
-// console.log(window);
-// window.prompt('correct?')
-// window.console.dir(window.document);
+let mapClicker = document.querySelector('.map');
+let popOver = document.querySelector(".popover");
+let body = document.querySelector("body");
 
-/* 1::  querySelector - uncomment in sequence */
-// var bodyElement = document.querySelector("body");
-/* view variable in console */
-// bodyElement.textContent = "Text content set in JavaScript";
-// bodyElement.style.borderBottom = "solid 30px red";
-/* elements in dev tools VS view source */
-/* comment out all the above */
+function handleClicks (event) {
+  event.preventDefault();
 
-/* 2::  querySelectorAll */
-// var listItems = document.querySelectorAll("li");
-/* view the variable in the console. Returns a NodeList */
+  console.log(event.target);
 
-/*  Gives an error - and the console log command never runs */
-// listItems.style.backgroundColor = "red";
-/* errors block the rest of the code from running */
-// console.log("This will not run b/c the line above causes a blocking error");
-
-/* HERE'S 3 methods that WOULD work */
-/* a. */
-// listItems.forEach(function (listItem) {
-//   listItem.style.backgroundColor = "red";
-// });
-
-/* b. */
-// for (var i of listItems) {
-//   i.style.backgroundColor = "green";
-// }
-
-/* c. */
-// for (var i = 0; i < listItems.length; i++) {
-//   listItems[i].style.backgroundColor = "blue";
-// }
-
-/* 3::  addEventListener */
-/* ADD a class of 'map' to the map ANCHOR tag in the sidebar */
-var mapLink = document.querySelector(".map");
-mapLink.style.borderBottom = "solid 8px red";
-
-// mapLink.addEventListener("click", function () {
-//   console.log("It works!");
-// });
-
-/* ADD a external link to the map ANCHOR tag in the sidebar */
-
-// mapLink.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log("It works!");
-// });
-
-// mapLink.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log("event:: ", event);
-//   console.log("event.target:: ", event.target);
-//   console.log("event.target.href:: ", event.target.href);
-// });
-
-// document.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   console.log("event.target::", e.target);
-//   console.log("event.targ  et.textContent::", e.target.textContent);
-// });
-
-/* 4:: FUNCTIONS */
-// var mapLink = document.querySelector(".map");
-// mapLink.addEventListener("click", buttonClick);
-
-// function buttonClick(e) {
-//   e.preventDefault();
-//   console.log("className::", e.target.className);
-//   e.target.className = "foo";
-// }
-
-/*
-.foo {
-  font-size: 200px;
-  color: rebeccapurple;
+  body.classList.toggle("showme");
+  
 }
-*/
 
-/* 5:: DYNAMICALLY CREATE AN ELEMENT */
-
-/* CREATE a div and then look at it in the console */
-// var newDiv = document.createElement("div");
-
-/* Add a class */
-// newDiv.className = "popover";
-// console.log(newDiv);
-
-/* Add text */
-// newDiv.innerText = "Hello World";
-
-/* Add styles to the div */
-// newDiv.style.fontSize = "40px";
-// newDiv.style.color = "#600";
-
-/* Store a reference to the H1 */
-// var headerOne = document.querySelector("header h1");
-
-/* Add the new div to the document */
-// headerOne.append(newDiv);
-// headerOne.prepend(newDiv);
+document.addEventListener ("click", handleClicks);
